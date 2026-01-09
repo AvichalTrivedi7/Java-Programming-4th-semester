4b1fd22dd1dfe8e2192b820a62ada838b6106467
AvichalTrivedi7 <avichaltrivedi7@gmail.com>
2026-01-09 10:41:45 +0530

    .

--- Program_4.java
+++ Program_4.java
@@ -11,6 +11,6 @@ public class Program_4 {
         Program_4 obj = new Program_4();
         int x = obj.myMethod(7);
         System.out.println(x);   // for non static functions you need to make objects to print it's value
-        System.out.println(myMethod2(7)); // for static functions you can directly call and print it's value
+        System.out.println(myMethod2(7)); // for static functions you can directly call and print it's value, wihtout making objects.
     }   
 }
