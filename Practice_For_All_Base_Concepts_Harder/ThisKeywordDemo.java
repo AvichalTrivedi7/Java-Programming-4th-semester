1f04213d20550fabc186a74aed934bf3b7bebafa
Avichal Trivedi <avichaltrivedi7@gmail.com>
2026-01-18 17:49:52 +0530

    .
    Removed duplicate main method in ThisKeywordDemo class.

--- Practice_For_All_Base_Concepts_Harder/ThisKeywordDemo.java
+++ Practice_For_All_Base_Concepts_Harder/ThisKeywordDemo.java
@@ -1,5 +1,12 @@
 package Practice_For_All_Base_Concepts_Harder;
 
+public class ThisKeywordDemo {
+    public static void main(String[] args) {
+        Students s = new Students(1, "Avichal");
+        s.display();
+    }
+}
+
 class Students {
     int id;
     String name;
@@ -13,11 +20,3 @@ class Students {
         System.out.println(id + " " + name);
     }
 }
-
-public class ThisKeywordDemo {
-    public static void main(String[] args) {
-        Students s = new Students(1, "Avichal");
-        s.display();
-    }
-}
-
