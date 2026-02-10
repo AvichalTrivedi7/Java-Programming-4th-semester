c8a3478fd6db1afc172896eeda4fd73f6d70b7d4
AvichalTrivedi7 <avichaltrivedi7@gmail.com>
2026-02-10 09:39:39 +0530

    .

--- Program_8.java
+++ Program_8.java
@@ -1,27 +1,28 @@
 // About constructor overloading 
 
-class Student{
+class Students{
     String name;
     int age;
     
     // Contructors are quite special they are only meant for assignment purposes. They do NOT EVEN have void return type, they just have no return. (Yes two different things) 
     // Constructor 1 (automatically done by jvm, we are just doing it to show how it works in the backend)
-    Student(){
+    Students(){
         name = " ";
         age = 0;
     }
 
     // Constructor 2
-    Student(String n){
+    Students(String n){
         name = n;
         age = 0;
     }
 
     // Constructor 3
-    Student(String n, int a){
+    Students(String n, int a){
         name = n;
         age = a;
     }
+
     void display(){
         System.out.println(name + " " + age);
     }
@@ -29,9 +30,9 @@ class Student{
 
 public class Program_8{
     public static void main(String[] args){
-        Student s1 = new Student();
-        Student s2 = new Student("Avi");
-        Student s3 = new Student("Avi", 21);
+        Students s1 = new Students();
+        Students s2 = new Students("Avi");
+        Students s3 = new Students("Avi", 21);
 
         s1.display();
         s2.display();
