3eef85a82422e08315fbd6aef4c641a102a158e2
AvichalTrivedi7 <avichaltrivedi7@gmail.com>
2026-03-20 10:06:55 +0530

    .

--- Programs_19and20/package_1/Animals.java
+++ Programs_19and20/package_1/Animals.java
@@ -1,10 +1,10 @@
 package Programs_19and20.package_1;
 
 public class Animals {
-
-    public void walk(){
+    protected void walk(){
         System.out.println("The animal is walking...");
     }
+    
     public static void main(String[] args) {
         Animals a1 = new Animals();
         a1.walk();
--- Programs_19and20/package_2/Pet.java
+++ Programs_19and20/package_2/Pet.java
@@ -3,6 +3,9 @@ package Programs_19and20.package_2;
 import Programs_19and20.package_1.Animals;
 
 class Dog extends Animals {
+    protected void walk() {
+        System.out.println("The Dog is walking...");
+    }    // we override because then only can it be used from parent class and be visible as a protected class to the object.
     protected void talk() {
         System.out.println("The Dog is talking...");
     }
